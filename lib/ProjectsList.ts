f0b65d67a22d15e4408659faf9479a6fdd0b5376
mikhael dantas <mikhael-dcs@outlook.com>
2023-01-06 23:35:01 -0300

    project updates

--- lib/ProjectsList.ts
+++ lib/ProjectsList.ts
@@ -10,9 +10,10 @@ type ProjectsList = {
 const GitHubProjectsList: ProjectsList = [
     {
         id: 'personal_landing_page',
-        image: 'https://media.discordapp.net/attachments/327238569569550338/1059593988623052921/logosemfundo.png',
+        // image: 'https://media.discordapp.net/attachments/327238569569550338/1059593988623052921/logosemfundo.png',
+        image: 'https://personal-site-gules-five.vercel.app/_next/image?url=%2Favatar.jpg&w=640&q=75',
         name: 'Personal Landing Page',
-        description: 'My personal landing page',
+        description: 'My personal website to present my professional profile and featured projects',
         url: 'https://www.github.com/mikhael-dantas/personal_landing_page',
         related: [],
         technologies: [
@@ -27,7 +28,7 @@ const GitHubProjectsList: ProjectsList = [
         id: 'e_commerce_simplified_api',
         image: 'https://media.discordapp.net/attachments/327238569569550338/1059593988623052921/logosemfundo.png',
         name: 'E-Commerce Simplified API',
-        description: 'A simplified e-commerce API',
+        description: 'Simplified e-commerce GraphQL API made with all the best practices and technologies',
         url: 'https://www.github.com/mikhael-dantas/e_commerce_simplified_api',
         related: [
             'e_commerce_simplified_frontend',
@@ -49,9 +50,9 @@ const GitHubProjectsList: ProjectsList = [
     },
     {
         id: 'e_commerce_simplified_frontend',
-        image: 'https://media.discordapp.net/attachments/327238569569550338/1059593988623052921/logosemfundo.png',
+        image: 'https://camo.githubusercontent.com/e3d4b8e739a10d265a665ff8bb6443f69686c7eec66f0f67c494bc962f454d29/68747470733a2f2f6d656469612e646973636f72646170702e6e65742f6174746163686d656e74732f3332373233383536393536393535303333382f313035393539333938393132323136363932352f6c6f676f2e706e67',
         name: 'E-Commerce Simplified Frontend',
-        description: 'A simplified e-commerce frontend',
+        description: 'Minified e-commerce frontend that consumes the GraphQL API resources',
         url: 'https://www.github.com/mikhael-dantas/e_commerce_simplified_frontend',
         related: [
             'e_commerce_simplified_api',
@@ -69,9 +70,9 @@ const GitHubProjectsList: ProjectsList = [
     },
     {
         id: 'tests_provider_front',
-        image: 'https://media.discordapp.net/attachments/327238569569550338/1059593988623052921/logosemfundo.png',
+        image: 'https://cdn-icons-png.flaticon.com/512/4016/4016758.png',
         name: 'Tests Provider Front',
-        description: 'A front-end for tests provider',
+        description: 'A front-end to build specifications based on business model to provide a structured object of use cases and functional requirements',
         url: 'https://www.github.com/mikhael-dantas/tests_provider_front',
         related: [
             'tests_provider_consumer_module',
@@ -85,9 +86,9 @@ const GitHubProjectsList: ProjectsList = [
     },
     {
         id: 'tests_provider_consumer_module',
-        image: 'https://media.discordapp.net/attachments/327238569569550338/1059593988623052921/logosemfundo.png',
+        image: 'https://cdn3.vectorstock.com/i/1000x1000/26/17/script-logo-design-vector-31372617.jpg',
         name: 'Tests Provider Consumer Module',
-        description: 'A module for tests provider',
+        description: 'A module that takes the specifications provided by the front-end and generates the tests based on the business model',
         url: 'https://www.github.com/mikhael-dantas/tests_provider_consumer_module',
         related: [
             'tests_provider_front',
